32a089724b2b5cde3d7a9f86320c000c43538baf
Mauro Bilotti <bilottim@knowfully.com>
2021-12-30 14:49:14 -0300

    First usage of component coming from bit.dev

--- hackathon-client/src/App.js
+++ hackathon-client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ButtonTest } from "@knowfully/hackathon-library.components.button-test";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Test of Components</p>
       </header>
+      <div>
+        <ButtonTest text={"Test from client"}></ButtonTest>
+      </div>
     </div>
   );
 }
